f381eaf4cc09200f0c0bb10223a48bf9ca73ca43
Kaivalya36 <K.chavan2@share.epsb.ca>
2025-04-29 13:31:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-
+//input
 document.getElementById("btn").addEventListener("click", cnv);
 function cnv() {
     var ftTemp = +document.getElementById("fa temp").value;
-
+//process
     let tempC = (ftTemp - 32) *(5 / 9);
+    //output
     document.getElementById("output").innerHTML = tempC;
     
 }
